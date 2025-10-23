78565d5377932e332aafe523e951e748a4e0ad23
commander-maker <nimeshdhalpegama@gmail.com>
2025-10-23 10:13:11 +0530

    App started
    App structure

---
+++ aqau_smarters/android/app/src/main/kotlin/com/example/aqau_smarters/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aqau_smarters
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ aqua_smatters/android/app/src/main/kotlin/com/example/aqua_smatters/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aqua_smatters
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
